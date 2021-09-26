866cdf50380faf5fe5b9b912887fb06e92d3f792
Valeriia Spasibukhova <megalera1913@gmail.com>
2021-09-26 11:18:15 +0300

    Добавьте файлы проекта.

---
+++ Book/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var title = new Title("Записки о Шерлоке Холмсе");
+            var author = new Author("Конан Дойл");
+            var content = new Content("Сборник из 11 детективных рассказов, написанных Артуром Конаном Дойлем, опубликованный в 1893 году");
+            Book book = new Book(author, title, content);
+            book.Show();
+            Console.ReadKey();
+        }
+    }
+    class Book
+    {
+        private readonly Author authorsName;
+        private readonly Title title;
+        private readonly Content content;
+
+        public Book(Author author, Title title, Content content)
+        {
+            this.authorsName = author;
+            this.title = title;
+            this.content = content;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Вся информация о книге: ");
+            Console.Write("\n");
+            authorsName.Show();
+            title.Show();
+            content.Show();
+
+        }
+    }
+    class Title
+    {
+        private readonly string name;
+
+        public Title(string titleName)
+        {
+            this.name = titleName;
+        }
+
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Название книги - {name}");
+        }
+    }
+    class Author
+    {
+        private readonly string bookAuthor;
+
+        public Author(string author)
+        {
+            this.bookAuthor = author;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Имя автора - {bookAuthor}");
+        }
+    }
+    class Content
+    {
+        private readonly string contents;
+
+        public Content(string bookcontent)
+        {
+            this.contents = bookcontent;
+        }
+
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Содержание книги - {contents}");
+        }
+    }
+}
---
+++ User/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Login = "user387";
+            user.Name = "Alex";
+            user.LastName = "Ivanov";
+            user.Age = 25;
+            user.Info();
+            Console.ReadKey();
+        }
+    }
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public readonly DateTime dateTime = new DateTime(2020, 12, 03);
+
+        public void Info()
+        {
+            Console.WriteLine($"Login --- {Login}");
+            Console.WriteLine($"Name --- {Name}");
+            Console.WriteLine($"LastName --- {LastName}");
+            Console.WriteLine($"Age --- {Age}");
+            Console.WriteLine($"DateTime --- {dateTime}");
+        }
+
+    }
+}
